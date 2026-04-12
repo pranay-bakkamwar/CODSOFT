5eec477616a747ec79d82432294f1834108704af
pranay-bakkamwar <pranaybakkamwar7905@gmail.com>
2026-04-12 15:39:45 +0530

    connect frontend to live backend

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
 });
 
 // Har request mein token automatically bhejne ke liye
